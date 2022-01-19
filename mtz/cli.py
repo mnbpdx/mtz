ae40a9764dbe3d4dc4a65ce14996be3126ab23bc
Mark N. Beebe <mbeebe08@gmail.com>
2022-01-19 00:10:41 -0800

    chore: play around with pygame.mixer

--- mtz/cli.py
+++ mtz/cli.py
@@ -1,24 +1,48 @@
+from time import sleep
 import click
-import podcastparser
-import urllib.request
+from pygame import mixer
 
+# mock state for now
+current_episode = 0
+current_position = 0
+
+# these will grab from cache/backend evenutally
+def get_current_episode(): return current_episode
+def get_current_position(): return current_position
 
 @click.group()
 def cli():
-    click.echo("no args")
-    feedurl = 'https://feeds.simplecast.com/ZL7iUDiH'
+    mixer.init()
 
-    parsed = podcastparser.parse(feedurl, urllib.request.urlopen(feedurl))
+    file = "../originals/101 - All Hail the Federated Alliance!-101-all-hail-the-federated-alliance_tc.mp3"
+    mixer.music.load(file)
 
-    file = open("feed.xml", "w")
-    file.write(parsed)
-    file.close()
+    mixer.music.play(start=0)
+    input(" ")
+    pause_position = mixer.music.get_pos()
+    click.echo(pause_position)
+    mixer.music.stop()
+    input(" ")
+    mixer.music.play(start=(pause_position / 1000))
+    click.echo(mixer.music.get_pos())
+    input(" ")
+    click.echo(mixer.music.get_pos())
+
+
 
 
 @cli.command()
-def play():
+def play(episode: int = 0, position: float = 0):
+
+    # TODO: queue up episode based on episode parameter
+    file = "../originals/101 - All Hail the Federated Alliance!-101-all-hail-the-federated-alliance_tc.mp3"
+    mixer.music.load(file)
+
+    mixer.music.play(start=position)
+
+
     click.echo("playing...")
 
 @cli.command()
-def play():
+def pause():
     click.echo("pausing...")
